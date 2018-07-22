e892ca612fa9f9910754ef8fb1ae89f67f775a62
Anton D <rus-21region@mail.ru>
2018-07-22 16:46:34 +0300

    Subscription

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -18,7 +18,7 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <?php foreach ($users as $user): ?>
-            <a href="<?php echo Url::to(['/user/profile/view', 'id' => $user->id]); ?>"><?php echo $user->username; ?></a>
+            <a href="<?php echo Url::to(['/user/profile/view', 'nickname' => $user->getNickName()]); ?>"><?php echo $user->username; ?></a>
             <hr>
         <?php endforeach; ?>
 
