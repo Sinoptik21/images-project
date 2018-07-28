58feff6a4011101bdf75aeb88f70a181fe78f9df
Anton D <rus-21region@mail.ru>
2018-07-28 22:31:48 +0300

    Admin:  configuring admin's actions

--- common/components/Storage.php
+++ common/components/Storage.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace frontend\components;
+namespace common\components;
 
 use Yii;
 use yii\base\Component;
@@ -58,7 +58,7 @@ class Storage extends Component implements StorageInterface
 
     $name = substr_replace($hash, '/', 2, 0);
     $name = substr_replace($name, '/', 5, 0);
-    return $name . '.' . $file->extension; // 0c/a9/277f91e40054767f69afeb0426711ca0fddd.jpg
+    return $name . '.' . $file->extension;
   }
 
   /**
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,10 +3,14 @@ return [
     'adminEmail' => 'admin@example.com',
 
     'maxFileSize' => 1024 * 1024 * 2,
-    'storagePath' => '@frontend/web/uploads/',
     'storageUri' => '/uploads/',
 
     'postPicture' => [
+        'maxWidth' => 1280,
+        'maxHeight' => 1024,
+    ],
+
+    'profilePicture' => [
         'maxWidth' => 1280,
         'maxHeight' => 1024,
     ],
